f721917a798935a290bdf0089d16fff9c1354187
Sreeram Panigrahi <panigrahi.sreeram@gmail.com>
2025-05-25 01:54:55 +0530

    use context and reducer to handle states

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,28 +1,57 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(""); // <- Change to string
 
-    const [text, setText] = useState('')
-    const [amount, setAmount] = useState(0)
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: Number(amount), // Convert to number on submit
+    };
+
+    addTransaction(newTransaction);
+    setText("");
+    setAmount(""); // Reset after adding
+  };
 
   return (
-    <> 
-    <h3>Add new transaction</h3>
-    <form id="form">
-      <div className="form-control">
-        <label htmlFor="text">Text</label>
-        <input type="text" value={text} onChange={(e) => {setText(e.taget.value)}} placeholder="Enter text..." />
-      </div>
-      <div className="form-control">
-        <label htmlFor="amount">Amount <br />
-          (negative - expense, positive - income)</label
-        >
-        <input type="number" value={text} onChange={(e) => {setAmount(e.taget.value)}} placeholder="Enter amount..." />
-      </div>
-      <button className="btn">Add transaction</button>
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)} // Leave as string
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn" type="submit">
+          Add transaction
+        </button>
       </form>
     </>
-  )
-}
+  );
+};
 
-export default AddTransaction
+export default AddTransaction;
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
-    <h3>History</h3>
-    <ul id="list" className="list">
-      <li className="minus">
-        Cash <span>-$400</span><button className="delete-btn">x</button>
-      </li>
-      <li className="plus">
-        Cash <span>+$300</span><button className="delete-btn">x</button>
-      </li>
-    </ul>
+      <h3>History</h3>
+      <ul id="list" className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default TransactionList
+export default TransactionList;
